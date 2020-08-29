6b5def698e2396854e00f3f6ffdc9c570dde72ee
hmasila <hannahmasila@gmail.com>
2020-08-29 09:57:20 +0300

    use v1 api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'users#home'
+  namespace :v1, defaults: { format: :json } do
+    resources :users, only: [:create, :show]
+  end
 end
