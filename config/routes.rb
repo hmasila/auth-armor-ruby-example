cfda10521d4bb32ffe910c97e1d95a0458a4d644
hmasila <hannahmasila@gmail.com>
2020-09-01 20:11:45 +0300

    add routes for autharmor

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'home#index'
   namespace :v1, defaults: { format: :json } do
-    resources :users, only: [:new, :create, :show]
+    namespace :auth do
+      scope "/autharmor" do
+        post '/invite', to: 'autharmor#invite'
+        post '/confirm', to: 'autharmor#confirm'
+        post '/auth', to: 'autharmor#auth'
+        get '/logout', to: 'autharmor#logout'
+        get '/me', to: 'autharmor#me'
+      end
+    end
   end
 end
