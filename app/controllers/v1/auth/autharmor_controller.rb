91dadac167511c318588f8352cdcbf566a7ef364
hmasila <hannahmasila@gmail.com>
2020-09-01 22:10:08 +0300

    add api endpoints for autharmor

---
+++ app/controllers/v1/auth/autharmor_controller.rb
@@ -0,0 +1,38 @@
+class V1::Auth::AutharmorController < ApplicationController
+	def invite
+    render json: {
+      success: "User found",
+    }, status: 200
+  end
+  
+  def logout
+    render json: {
+      success: "User found",
+    }, status: 200
+  end
+  
+  def auth
+    render json: {
+      success: "User found",
+    }, status: 200
+  end
+  
+  def me
+    render json: {
+      success: "User found",
+    }, status: 200
+	end
+
+  def confirm
+    @user = User.find_by(username: params[:username])
+    if @user.present?
+      render json: {
+        success: "User found",
+      }, status: 200
+    else
+      render json: {
+        error: "No such user; check the submitted username",
+      }, status: 404
+    end
+	end
+end
