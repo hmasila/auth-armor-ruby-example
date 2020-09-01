02f3fbe7f6349ceba4276baae18ac45a5ff9298f
hmasila <hannahmasila@gmail.com>
2020-09-01 22:27:16 +0300

    add password

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2020_08_29_061925) do
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
+    t.string "password_digest"
     t.string "username", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
