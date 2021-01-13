a6314261853a35b4435c883d7ca01f71de06ff59
msabeni <kemimsabeni@gmail.com>
2021-01-13 13:13:51 +0300

    tiny mods

--- db/migrate/20210112151934_create_users.rb
+++
@@ -1,6 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users do |t|
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_829_061_925) do
+ActiveRecord::Schema.define(version: 20_210_113_094_255) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
