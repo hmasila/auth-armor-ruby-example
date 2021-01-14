c53a3898cc43451fba1a30f4647396942bf3c30a
hmasila <hannahmasila@gmail.com>
2021-01-14 11:35:57 -0600

    remove duplicate migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_094255) do
+ActiveRecord::Schema.define(version: 2020_08_29_061925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.string "email", null: false
-    t.integer "age", null: false
-    t.string "gender", default: ""
+    t.string "nickname"
+    t.string "password_digest"
+    t.string "username", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
