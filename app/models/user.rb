33b5037375ec7da49d88bd75a1457167b69c6418
hmasila <hannahmasila@gmail.com>
2021-01-14 11:41:50 -0600

    remove changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,2 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-
-  GENDERS = ['male', 'female']
-
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :age, presence: true
-
-  # validates gender includes GENDERS if gender is provided
 end
