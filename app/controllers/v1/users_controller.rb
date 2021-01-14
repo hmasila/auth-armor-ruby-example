ec767b3d8700c46862efa04e44f9ab79bba512b8
Hannah Masila <hannahmasila@gmail.com>
2021-01-14 12:26:57 -0600

    Revert "Rubocop"

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,22 +1,18 @@
-# frozen_string_literal: true
+class V1::UsersController < ApplicationController
+	def create
+	  User.create!(username: params[:username])
+	end
 
-module V1
-  class UsersController < ApplicationController
-    def create
-      User.create!(username: params[:username])
+  def show
+    @user = User.find_by(username: params[:username])
+    if @user.present?
+      render json: {
+        success: "User found",
+      }, status: 200
+    else
+      render json: {
+        error: "No such user; check the submitted username",
+      }, status: 404
     end
-
-    def show
-      @user = User.find_by(username: params[:username])
-      if @user.present?
-        render json: {
-          success: 'User found'
-        }, status: 200
-      else
-        render json: {
-          error: 'No such user; check the submitted username'
-        }, status: 404
-      end
-    end
-  end
+	end
 end
